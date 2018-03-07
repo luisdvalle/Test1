5e21f8858c63e0f1feb07ca30d39e418d4781cf5
luisdvalle <jums.delvalle@gmail.com>
2018-03-07 19:17:12 +1100

    Adding Interface for service
    Providing an interface with all the required methods to get and set
    entities

---
+++ AnsibleSolution/Abstractions/IMyEntityService.cs
@@ -0,0 +1,16 @@
+﻿using AnsibleSolution.Models;
+using System.Collections.Generic;
+
+namespace AnsibleSolution.Abstractions
+{
+    public interface IMyEntityService
+    {
+        IEnumerable<MyEntity> GetAllEntities();
+
+        MyEntity GetEntityById(int id);
+
+        void AddEntity(MyEntity entity);
+
+        void AddListEntitites(IEnumerable<MyEntity> entities);
+    }
+}
