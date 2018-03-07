cc7a8deea83e346d6dec07cb07e798eda19a21cf
luisdvalle <jums.delvalle@gmail.com>
2018-03-07 19:20:34 +1100

    Adding MyEntityService concrete class
    Initial commit for a class implementing IMyEntityService interface

---
+++ AnsibleSolution/Services/MyEntityService.cs
@@ -0,0 +1,30 @@
+﻿using AnsibleSolution.Abstractions;
+using AnsibleSolution.Models;
+using System;
+using System.Collections.Generic;
+
+namespace AnsibleSolution.Services
+{
+    public class MyEntityService : IMyEntityService
+    {
+        public void AddEntity(MyEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddListEntitites(IEnumerable<MyEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<MyEntity> GetAllEntities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MyEntity GetEntityById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
