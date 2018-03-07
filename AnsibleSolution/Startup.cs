35e5cbdd1e6d95afe30862108fee34c83318ce00
luisdvalle <jums.delvalle@gmail.com>
2018-03-07 17:57:15 +1100

    Initial Startup configuration
    Adding middlewares to the pipeline and adding MVC to DI container

--- AnsibleSolution/Startup.cs
+++ AnsibleSolution/Startup.cs
@@ -15,6 +15,7 @@ namespace AnsibleSolution
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +26,9 @@ namespace AnsibleSolution
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            // Adding middlewares to the pipeline
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
