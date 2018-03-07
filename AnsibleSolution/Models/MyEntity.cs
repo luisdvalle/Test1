b4b6d4583af1c5816a3e40d858ce99316b128e26
luisdvalle <jums.delvalle@gmail.com>
2018-03-07 18:58:11 +1100

    Adding MyEntity Model class
    POCO class MyEntity to map the data source

---
+++ AnsibleSolution/Models/MyEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnsibleSolution.Models
+{
+    public class MyEntity
+    {
+        public int MyEntityId { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+    }
+}
